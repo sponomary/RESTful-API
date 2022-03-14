56d4a0dcd20471c8ae3bdeb5634506301b8582aa
sponomary <alex.ponomaryova@gmail.com>
2022-03-14 22:30:28 +0100

    Toy BDD added 2v.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataViewer.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
