9f04e745e6134f1cbc287fafda8704c806ec1838
eliselinc <101522264+eliselinc@users.noreply.github.com>
2022-03-21 15:26:00 +0100

    requête multiple + scheduler

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from __future__ import unicode_literals
     :copyright: © 2022 by Élise & Lufei & Alexandra.
 """
 
-__version__ = "0.4"
+__version__ = "0.5"
 
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_apscheduler import APScheduler #ajouter dans les requirements
 from datetime import datetime
-import json
 from dataclasses import dataclass
 
 app = Flask(__name__)
@@ -26,8 +26,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/DataViewer.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.update({'SCHEDULER_API_ENABLED': True})
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+# Créer un objet Scheduler pour une tâche programmée
+scheduler=APScheduler()
+scheduler.init_app(app)
+
+# Ajouter une tâche programmée à l'ordonnanceur (mise à jour des données depuis datagouv + mise à jour de base de données)
+@scheduler.task('interval', id='do_job', days=1)
+def job():
+    print("Synchronisation...")
+    synchro.update_db(synchro.update_data_covid("https://datavaccin-covid.ameli.fr/api/v2/catalog/datasets/donnees-de-vaccination-par-commune/exports/json?limit=-1&offset=0&timezone=UTC", "data/donnees-de-vaccination-par-commune.json")) #update_db(update_data_covid(url_ameli, covid_json))
+    print("...synchronisation terminée")
+
+# Démarrer le travail du planificateur de tâches programmé 
+scheduler.start()
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -315,7 +332,7 @@ def data_filter():
     return jsonify(data_covid_schema.dump(results))
 
     """
-    Même programme mais en listant tous les paramètres !
+    Même fonction mais en listant tous les paramètres !
 
     # récupérer les paramètres spécifiés par l'utilisateur et stocker dans un variable
     query_parameters = request.args
@@ -387,7 +404,7 @@ def update_covid(id):
     return data_covid_schema.jsonify(data_covid)
 
 
-# Route qui supprimer une donnée covid ✅
+# Route qui supprime une donnée covid ✅
 @app.route('/covid/<int:id>/', methods=["DELETE"])
 def delete_covid(id):
     data_covid = DATA_COVID.query.get(id)
@@ -398,6 +415,6 @@ def delete_covid(id):
 
 
 if __name__ == "__main__":
-    # si jamais le port bugge, connecter àun autre
+    # si jamais le port bugge, connecter à un autre
     # app.run(host='127.0.0.1', port=5001)
     app.run(debug=True)
