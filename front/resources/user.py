9e545af66773be26c05e79a5adc9e5ef21407beb
lufeiliu <72169635+lufeiliu@users.noreply.github.com>
2022-04-07 21:35:23 +0200

    Add files via upload

--- front/resources/user.py
+++ front/resources/user.py
@@ -7,10 +7,29 @@ from datetime import datetime, timedelta
 import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 
-from front.models.api import login_api
+from front.models.api import login_api,create_client
 
 
 users = Blueprint('users', __name__)
+
+@users.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        data = dict(request.form)
+        api_resp = create_client(data)
+        print(api_resp)
+        code_resp = api_resp[0]
+        data_response = api_resp[1]
+
+        if code_resp != 201 :
+            flash("error: %s"%code_resp)
+            render_template('user/README.html', api_error=True,
+                            api_message=data_response['message'])
+        else:
+            return ("mettre la page login")
+            #return redirect(url_for('authcepty.login'), api_error=False)
+
+    return render_template('user/register.html')
 
 
 @users.route('/login', methods=('GET', 'POST'))
@@ -20,6 +39,7 @@ def login():
         username = request.form['email'] 
         password = request.form['password']
         api_resp = login_api(username, password)
+        print(api_resp)
         code_resp = api_resp[0]
         data_response = api_resp[1]
 
@@ -43,7 +63,7 @@ def login():
 
     elif request.method == 'GET':
         print("une mini étape réussi ! ")
-        return render_template('test/README.html', api_error=False)
+        return render_template('user/README.html', api_error=False)
     else: flash(error)
 
 
