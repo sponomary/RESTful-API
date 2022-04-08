7ecee6cf7c0849d813dc2e77834dee482eefd35c
lufeiliu <72169635+lufeiliu@users.noreply.github.com>
2022-04-09 01:16:07 +0200

    réécrire quelques messages en français

--- back/resources/user.py
+++ back/resources/user.py
@@ -1,14 +1,15 @@
 import email
 from flask import Blueprint
 from ..models.user import User
-from flask import request, make_response
+from flask import request, jsonify, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from datetime import datetime, timedelta
 
 users = Blueprint('users', __name__)
 
-# Inscription
+
+# Création d'un compte
 @users.route('/register', methods=['POST'])
 def register():
     # Création d'un dictionnaire pour stocker les form-data
@@ -34,16 +35,17 @@ def register():
             'exp': datetime.utcnow() + timedelta(minutes=30)}, 
             "secret_key_data_covid")
         return make_response(
-            {'message':'Successfully registered.', 
+            {'message':'Inscription réussie.', 
             'status':201,
             'token': token.decode('UTF-8'), 
             'email':email})
     else:
         # Code 202, un utilisateur avec cet email existe déjà
         return make_response(
-            {'message':'User already exists. Please Log in.', 
+            {'message':"L'utilisateur existe déjà. Veuillez vous connecter.", 
             'status':202, 
             'email':email})
+
 
 # Connexion
 @users.route('/login', methods=['POST'])
@@ -53,21 +55,16 @@ def login():
     if not auth or not auth.get('email') or not auth.get('password'):
         # Code 401, email et/ou mot de passe non saisi
         return make_response(
-            {'message':'Login required',
+            {'message':'Veuillez vous connecter',
             'status':401,
             'user':auth.get(email)})
-        """🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽 QUE FAIRE DE CA ?
-        {'WWW-Authenticate': 'Basic realm ="Login required !!"'}"""
     user = User.query.filter_by(email=auth.get('email')).first()
     if not user:
         # Code 401, l'utilisateur n'existe pas dans la BDD
         return make_response(
-            {'message':'User does not exist',
+            {'message':'Utilisateur non trouvé, veuillez vous inscrire',
             'status':401,
             'user':auth.get(email)})
-        """🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽 QUE FAIRE DE CA ?
-        {'WWW-Authenticate': 'Basic realm ="User does not exist !!"'}"""
-
     if check_password_hash(user.password, auth.get('password')):
         # Génération du JWT Token
         token = jwt.encode(
@@ -75,14 +72,13 @@ def login():
             'exp': datetime.utcnow() + timedelta(minutes=30)}, 
             "secret_key_data_covid")
         return make_response(
-            {'message':'Successfully logged in',
+            {'message':'Connexion réussie',
             'status':201,
             'token': token.decode('UTF-8'),
             'user':auth.get(email)})
     # Code 403, mot de passe incorrect
     return make_response(
-            {'message':'Wrong password',
+            {'message':'Mot de passe erroné',
             'status':403,
             'user':auth.get(email)})
-    """🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽🐽 QUE FAIRE DE CA ?
-    {'WWW-Authenticate': 'Basic realm ="Wrong Password !!"'}"""
+
