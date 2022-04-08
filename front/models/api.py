e0c96f4dea24d9b9a917f512226336ecff73961b
lufeiliu <72169635+lufeiliu@users.noreply.github.com>
2022-04-08 18:22:49 +0200

    petits ajouts

--- front/models/api.py
+++ front/models/api.py
@@ -17,17 +17,18 @@ from os import environ
 # -----------------------------------------------------------------------
 
 HOST_API = "http://dataviewer.api.localhost:5000/api"
+#HOST_API = "http://127.0.0.1:5000/api"
 URL_LOGIN = HOST_API+"/login"
 URL_USER_POST = HOST_API+"/register"
 URL_ALLDATA_GET = HOST_API+"/covid/" # URL qui retourne toutes les données covid 
 URL_ONEDATA_GET_PATCH_DELETE = HOST_API+"/covid/{data_id}/" # URL qui retourne/met à jour/supprime une donnée covid
 URL_ONEDATA_POST = HOST_API+"/covid/" # URL qui crée une nouvelle donnée covid 
 URL_UNIQUE_INFO_GET = HOST_API+"/covid/{info}/" # URL qui permet de retourner toutes les données uniques de certains colonnes 
-URL_MULTIPLE_INFO_GET = HOST_API+"/covid/search" # URL qui renvoie les résultats d'une requête (simple ou multiple)
+URL_MULTIPLE_INFO_GET = HOST_API+"/covid/search/?{info}" # URL qui renvoie les résultats d'une requête (simple ou multiple)
 
 # -----------------------------------------------------------------------
 
-__all__ = ['login_api']
+__all__ = ['login_api','create_client','get_all_covid','get_covid_by_id','update_covid','delete_covid','get_multiple_info']
 
 # -----------------------------------------------------------------------
 
@@ -41,13 +42,14 @@ def create_client(data):
 
 def get_all_covid():
     resp_api = requests.get(URL_ALLDATA_GET)
-    return resp_api.json()
+    return resp_api.status_code, resp_api.json()
 
 def get_covid_by_id(id):
     print("GET COVID ID "+id)
     resp_api = requests.get(URL_ONEDATA_GET_PATCH_DELETE.format(data_id=id))
     print("RESP:",resp_api.json())
-    return resp_api.json()
+    print("code:",resp_api.status_code)
+    return resp_api.status_code, resp_api.json()
 
 def update_covid(id):
     print("UPDATE COVID ID "+id)
@@ -64,5 +66,12 @@ def delete_covid(id):
 # URL_ONEDATA_POST ---> créer une nouvelle donnée covid
 
 # URL_MULTIPLE_INFO_GET ---> faire + tard car implique un nouveau formulaire HTML
+def get_multiple_info(search_info):
+    print("Info to search "+search_info)
+    resp_api = requests.get(URL_MULTIPLE_INFO_GET.format(info = search_info))
+    print("RESP:",resp_api.json())
+    print("CODE",resp_api.status_code)
+    return resp_api.status_code,resp_api.json()
+
 
 # URL_UNIQUE_INFO_GET ---> faire + tard car implique un nouveau tableau HTML et modifs jinja
