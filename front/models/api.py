d884d950a30b6a9a603245266997af2322b21253
lufeiliu <72169635+lufeiliu@users.noreply.github.com>
2022-04-09 17:40:55 +0200

    modifications "get_all_covid"

--- front/models/api.py
+++ front/models/api.py
@@ -20,7 +20,8 @@ HOST_API = "http://dataviewer.api.localhost:5000/api"
 #HOST_API = "http://127.0.0.1:5000/api"
 URL_LOGIN = HOST_API+"/login"
 URL_USER_POST = HOST_API+"/register"
-URL_ALLDATA_GET = HOST_API+"/covid/" # URL qui retourne toutes les données covid 
+#URL_ALLDATA_GET = HOST_API+"/covid/" # URL qui retourne toutes les données covid 
+URL_ALLDATA_GET = HOST_API+"/covid/?{data_range}" # URL qui retourne toutes les données covid par paquet 
 URL_ONEDATA_GET_PATCH_DELETE = HOST_API+"/covid/{data_id}/" # URL qui retourne/met à jour/supprime une donnée covid
 URL_ONEDATA_POST = HOST_API+"/covid/" # URL qui crée une nouvelle donnée covid 
 URL_UNIQUE_INFO_GET = HOST_API+"/covid/{info}/" # URL qui permet de retourner toutes les données uniques de certains colonnes 
@@ -41,9 +42,16 @@ def create_client(data):
     return resp_api.json()
 
 # PB : fait freeze le pc car trop de données :(
-def get_all_covid():
-    resp_api = requests.get(URL_ALLDATA_GET)
+def get_all_covid(data_range):
+    # il faut que url passé à requests.get ressemble à ca : http://dataviewer.api.localhost:5000/api/covid/?start=1&limit=5
+    print("REQUEST : "+URL_ALLDATA_GET.format(data_range = data_range))
+    resp_api = requests.get(URL_ALLDATA_GET.format(data_range = data_range))
+    print("CODE:",resp_api.status_code)
+    print("RESP:",resp_api.json())
     return resp_api.status_code, resp_api.json()
+    #resp_api = requests.get(URL_ALLDATA_GET)
+    #return resp_api.status_code, resp_api.json()
+
 
 # (?) peut-être on peut zapper ça car se fait direct dans la recherche si on remplit que le champ id (?) à voir
 # Je suis d'accord pour enlever ça, ça rend plus prore ---lulu
