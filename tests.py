9bce35b2827b323e00cdc3436e00ae73071bcc13
eliselinc <linckerelise@gmail.com>
2022-04-10 22:34:46 +0200

    test unit tests

--- tests.py
+++ tests.py
@@ -33,9 +33,54 @@ class TestURLs(unittest.TestCase):
         from back.app import dataviewerBack as backend
 
         tester = backend.test_client(self)
-        response = tester.post('/register', data={'email':'abc@163.com', 'password':'1234'})
+        response = tester.post('/login', data={'email':'abc@163.com', 'password':'1234'})
         self.assertEqual(response.status_code, 200)
     
+    def test_register(self):
+        from back.app import dataviewerBack as backend
+
+        tester = backend.test_client(self)
+        response = tester.post('/register', data={'name':'Toto', 'email':'toto@163.com', 'password':'1234'})
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_all_data(self):
+        from back.app import dataviewerBack as backend
+
+        tester = backend.test_client(self)
+        response = tester.get('/covid/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_create_data(self):
+        from back.app import dataviewerBack as backend
+
+        tester = backend.test_client(self)
+        response = tester.post('/covid/',
+        #AJOUTER UN HEADERS ? POUR LE TOKEN ?
+        data={
+            "classe_age": "65-74",
+            "commune_residence": 444,
+            "date_reference": "2022-03-22",
+            "effectif_1_inj": 0,
+            "effectif_cumu_1_inj": 1750,
+            "effectif_cumu_termine": 1730,
+            "effectif_termine": 0,
+            "libelle_classe_age": "de 65 à 74 ans",
+            "libelle_commune": "SASHAVILLE",
+            "population_carto": 680,
+            "semaine_injection": "2021-44",
+            "taux_1_inj": 0.018,
+            "taux_cumu_1_inj": 0.747,
+            "taux_cumu_termine": 0.823,
+            "taux_termine": 0.010
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_one_data_from_id(self):
+        from back.app import dataviewerBack as backend
+
+        tester = backend.test_client(self)
+        response = tester.get('/covid/512/') # OK MARCHE MAIS PAS SURE SI ON PEUT LAISSER LE CHIFFRE COMME CA !!!!!!!!!!!!!!!!!!!
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main(verbosity=8)
